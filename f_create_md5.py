c02550d30cbf82653a2eca3b14b608401dca4bd9
rajkogojkovic <gojkovic.rajkogmail.com>
2019-04-25 09:13:21 +0200

    accepted comment

--- f_create_md5.py
+++ f_create_md5.py
@@ -1,6 +1,6 @@
 from hashlib import md5
 '''
-function takes one input parameter and returns hexdigest value
+function takes one input parameter and returns hexdigest value as converted text
 '''
 def create_md5(in_string):
     v_md5=md5()
